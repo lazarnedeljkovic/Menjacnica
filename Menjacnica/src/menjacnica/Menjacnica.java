204f28304eb14e3c797381aada2d761d9138d6a7
lazarnedeljkovic <lazar.nedeljkovic84@gmail.com>
2015-03-29 20:48:43 +0200

    U klasi Menjacnica implementirana je samo metoda dodajKurs.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -6,21 +6,40 @@ import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs{
 
+	private Evro evro;
+	
 	public void dodajKurs(double prodajniKurs, double kupovniKurs,
 			double srednjiKurs, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+		if (datum!=null) {
+			try {
+				evro.setDatum(datum);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				evro.setProdajniKurs(prodajniKurs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				evro.setKupovniKurs(kupovniKurs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				evro.setSrednjiKurs(srednjiKurs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public String vratiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-
-	
 }
