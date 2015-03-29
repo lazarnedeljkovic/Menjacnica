d6697ada0821e039f66903944874ac76685290fb
lazarnedeljkovic <lazar.nedeljkovic84@gmail.com>
2015-03-29 19:33:55 +0200

    U klasi Evro su dodate equals, hashCode, i toString metode.

--- Menjacnica/src/menjacnica/Evro.java
+++ Menjacnica/src/menjacnica/Evro.java
@@ -46,5 +46,47 @@ public class Evro {
 	public void setDatum(GregorianCalendar datum) {
 		
 		this.datum = datum;
-	}	
+	}
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evro other = (Evro) obj;
+		if (Double.doubleToLongBits(kupovniKurs) != Double
+				.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double
+				.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double
+				.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+	public String toString() {
+		return "Naziv: " + naziv + ",  Prodajni kurs: " + prodajniKurs
+				+ ",  Kupovni kurs: " + kupovniKurs + ",  Srednji kurs: "
+				+ srednjiKurs + ",  Datum: " + datum;
+	}
 }
