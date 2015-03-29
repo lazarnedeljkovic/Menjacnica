f6ce076b62b36e3c5f0acec2827be96528ee6246
lazarnedeljkovic <lazar.nedeljkovic84@gmail.com>
2015-03-29 20:24:46 +0200

    U klasi Menjacnica su implementirane dodajKurs, obrisiKurs, i vratiKurs metode.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -5,20 +5,59 @@ import java.util.GregorianCalendar;
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs{
+	
+	private Evro evro;
 
 	public void dodajKurs(double prodajniKurs, double kupovniKurs,
 			double srednjiKurs, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+		try {
+			evro.setProdajniKurs(prodajniKurs);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			evro.setKupovniKurs(kupovniKurs);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			evro.setSrednjiKurs(srednjiKurs);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			evro.setDatum(datum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+		if (evro.getDatum().equals(datum)) {
+			try {
+				evro.setProdajniKurs(0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				evro.setKupovniKurs(0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				evro.setSrednjiKurs(0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public String vratiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		if (evro.getDatum().equals(datum)) {
+			return "Prodajni kurs: "+evro.getProdajniKurs()+",  Kupovni kurs: "+
+					evro.getKupovniKurs()+",  Srednji kurs: "+evro.getSrednjiKurs();
+		}
+		
 		return null;
 	}
 
